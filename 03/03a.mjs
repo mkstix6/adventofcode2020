13d1b817c63eee510a69ca948d8ba5506efd3a9f
Mark Stickling <mkstix6@gmail.com>
2020-12-12 16:29:02 +0000

    Solution for 03a

--- 03/03a.mjs
+++ 03/03a.mjs
@@ -1,17 +1,27 @@
+/**
+ * Starting at the top-left corner of your map and following
+ * a slope of right 3 and down 1, how many trees would
+ * you encounter?
+ */
 import fs from "fs";
 const data = fs.readFileSync("mapPattern.txt", "utf8");
 const lines = data.split(/\r?\n/);
 
-// Trees
-const treeHits = lines.map((line) => {
-  let position = 0;
-  const hits = [];
-  [...line].forEach((character) => {
-    position++;
-    if (character === "#") {
-      hits.push(position);
-    }
-  });
-  return hits;
-});
-console.log(treeHits[0]);
+function checkTreeHit(pattern, index) {
+  const progression = [1, 3];
+  const patternLength = lines[0].length;
+  const charToCheck = (index * progression[1]) % patternLength;
+  if (pattern.charAt(charToCheck) === "#") {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+const sum = (...args) => [...args].reduce((acc, val) => acc + val, 0);
+
+const hits = sum(
+  ...lines.map(checkTreeHit).map((boolean) => (boolean ? 1 : 0))
+);
+
+console.log({ hits });
