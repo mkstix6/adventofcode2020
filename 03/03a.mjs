84e37bd2ea9d78c8ce1cd72b3cc6c589064cd4dc
Mark Stickling <mkstix6@gmail.com>
2020-12-13 21:32:58 +0000

    Solution for 03b

--- 03/03a.mjs
+++ 03/03a.mjs
@@ -3,25 +3,45 @@
  * a slope of right 3 and down 1, how many trees would
  * you encounter?
  */
+
 import fs from "fs";
 const data = fs.readFileSync("mapPattern.txt", "utf8");
 const lines = data.split(/\r?\n/);
 
-function checkTreeHit(pattern, index) {
-  const progression = [1, 3];
-  const patternLength = lines[0].length;
-  const charToCheck = (index * progression[1]) % patternLength;
-  if (pattern.charAt(charToCheck) === "#") {
-    return true;
-  } else {
-    return false;
-  }
-}
+const progressions = [
+  [1, 1],
+  [1, 3],
+  [1, 5],
+  [1, 7],
+  [2, 1],
+];
+
+const getCheckTreeHitCheckerForProgression = (progression) => {
+  return (pattern, index) => {
+    const patternLength = lines[0].length;
+    const charToCheck = (index * progression[1]) % patternLength;
+    if (pattern.charAt(charToCheck) === "#") {
+      return true;
+    } else {
+      return false;
+    }
+  };
+};
 
 const sum = (...args) => [...args].reduce((acc, val) => acc + val, 0);
 
-const hits = sum(
-  ...lines.map(checkTreeHit).map((boolean) => (boolean ? 1 : 0))
-);
+const pregressionHits = progressions.map((progression) => {
+  const checkTreeHit = getCheckTreeHitCheckerForProgression(progression);
+  // Filter lines down to just the ones we want to check.
+  const lineSkip = progression[0];
+  let linesToCheck = lines.filter((line, index) => !(index % lineSkip));
+  // Count hits.
+  const hits = sum(
+    ...linesToCheck.map(checkTreeHit).map((boolean) => (boolean ? 1 : 0))
+  );
+  return hits;
+});
 
-console.log({ hits });
+const reduceProduct = (acc, val) => acc * val;
+const finalProduct = pregressionHits.reduce(reduceProduct);
+console.log(finalProduct);
