48a863b02439401999adb9d8e6df145487e5a40f
Mark Stickling <mkstix6@gmail.com>
2020-12-15 23:28:21 +0000

    Solution for 05a

--- 05py/main.py
+++ 05py/main.py
@@ -1,17 +1,34 @@
-# Function version
-# def countUniqueLetters(groupString):
-# 	return len(set([char for char in groupString]))
-# Lambda version
-countUniqueLetters = lambda groupString : len(set([char for char in groupString]))
+inputFile = 'day5-puzzle-data.txt'
+# inputFile = 'example-data.txt'
 
-##########
 
-# Import file data
+def countStringUniqueLetters(groupString): return len(
+    set([char for char in groupString]))
 
-# Put groups into array.
 
-####### For each group #####
-# 
-groupString = 'aabccd'
+# Import puzzle file data
+with open(inputFile) as f:
+    allData = f.readlines()
 
-print(countUniqueLetters(groupString))
+# Create array of each group's answers
+groups = []
+group = ''
+for x in range(len(allData)):
+    person = allData.pop(0)
+    person = person.strip('\n')
+    if person == '':
+        if group != '':
+            groups.append(group)
+            group = ''
+    else:
+        group += person
+    if len(allData) == 0:
+        groups.append(group)
+
+# Count total unique yes answers from each group
+totalYesAnswers = 0
+for g in groups:
+    totalYesAnswers += countStringUniqueLetters(g)
+
+# Print puzzle-day-05a answer
+print(totalYesAnswers)
