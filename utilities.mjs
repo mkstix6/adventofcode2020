4cd54ff06ed7a114cf865985c33cc93d6366af0e
Mark Stickling <mkstix6@gmail.com>
2020-12-28 23:00:48 +0000

    Add utility function to get a file's lines into an array

--- utilities.mjs
+++ utilities.mjs
@@ -1,8 +1,16 @@
 import fs from "fs";
 
+const regexEndOfLineNewLine = /\r?\n/;
+
 const getFileData = (fileLocation) => {
   const fileTextData = fs.readFileSync(fileLocation, "utf8");
   return fileTextData;
 };
 
-export { getFileData };
+const getFileLinesIntoArray = (fileLocation) => {
+  const wholeFile = getFileData(fileLocation);
+  const arrayOfLines = wholeFile.split(regexEndOfLineNewLine);
+  return arrayOfLines;
+};
+
+export { getFileData, getFileLinesIntoArray };
