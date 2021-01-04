8c2ba9ce7910c41589473191e34d95c03355e77e
Mark Stickling <mkstix6@gmail.com>
2021-01-04 15:04:11 +0000

    Save answer for 05b

--- 05/05.ts
+++ 05/05.ts
@@ -7,8 +7,24 @@ const lines = data.split(/\r?\n/);
 const seatProperties = lines.map(seatCodeToProperties)
 const seatIDs = seatProperties.map(({id}) => id)
 // Find max
-const largest = Math.max(...seatIDs)
-const answer = largest
+const largestSeatID = Math.max(...seatIDs)
+const answerPartA = largestSeatID
 
-console.log('Answer for 05a: ', answer)
+console.log('Answer for 05a: ', answerPartA);
 // Your puzzle answer was 928.
+
+// Find missing numbers.
+const sortedSeatIDs = new Set([...seatIDs].sort((a,b) => a-b))
+const possibleSeats = []
+for(let $i = 0; $i < 1030; $i++){
+  if(sortedSeatIDs.has($i-1) && !sortedSeatIDs.has($i) && sortedSeatIDs.has($i+1)) {
+    possibleSeats.push($i)
+  }
+}
+
+if (possibleSeats.length === 1) {
+  console.log('Answer for 05b: ', possibleSeats[0]);
+  // Your puzzle answer was 610.
+} else {
+  console.error(`Multiple possible answers identified; there should only be one answer but we found ${possibleSeats.length}.`)
+}
